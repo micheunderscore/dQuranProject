c4193bc5d0eb19ed9d6270941dddc2c145fc34b0
achengg <97327123+ach3ngg@users.noreply.github.com>
2022-01-23 20:29:20 +0800

    Update on LP
    Did some work on screen management.

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float length, startPosition;
-    public GameObject camera;
+    private float length, startPos;
+    public GameObject cam;
     public float parallaxEffect;
 
     void Start()
     {
-        startPosition = transform.position.x;
+     startPos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float temp = (camera.transform.position.x*(1 - parallaxEffect));
-        float distance = (camera.transform.position.x * parallaxEffect);
+        float temp = (cam.transform.position.x*(1 - parallaxEffect));
+        float distance =  (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+        transform.position = new Vector3 (startPos + distance, transform.position.y, transform.position.z);
     
-        if(temp > startPosition+ length) startPosition += length;
-        else if (temp < startPosition - length) startPosition-= length;
+        if (temp > startPos + length) startPos += length;
+        else if  (temp < startPos - length) startPos-= length;
     }
 }
