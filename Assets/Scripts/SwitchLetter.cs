3f26c837e0aaf1341106e1ec0648a7478068c92a
achengg <97327123+ach3ngg@users.noreply.github.com>
2022-02-14 19:17:54 +0800

    Save and Load Almost but not Quite
    -  save and load on playerpref with collider as trigger..
    - i didnt load the player pref on start function, it messed up my other void function..but the problem is..if i didnt call on start function, whenever the scene is reloading...the object doesnt read the index from playerpref, hence didnt show the user (star) progression.

--- Assets/Scripts/SwitchLetter.cs
+++ Assets/Scripts/SwitchLetter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 
 public class SwitchLetter : MonoBehaviour
@@ -10,21 +11,29 @@ public class SwitchLetter : MonoBehaviour
 
     public GameObject[] hijaiyahLetter;
     public GameObject[] hijaiyahTitleText; 
-    public AudioSource[] hijaiyahLetterSound;
+    public GameObject[] hijaiyahLetterSound;
     public GameObject[] letterNavigationBtn;
     public GameObject[] prefabLetter;
+    public VideoPlayer[] recitationLetterVideo;
+    public GameObject[] progressDialogueBox;
+    public GameObject[] progressStarLetter;
 
     int indexLetter;
+    int indexPrevious;
+    public int newIndexTrigger = 0;
 
     void Start()
-    {
+    {   
         indexLetter = 0;
         hijaiyahLetter[0].gameObject.SetActive(true);
         hijaiyahTitleText[0].gameObject.SetActive(true);
     }
 
-    void UpdateLetter()
+    void Update()
     {
+
+        
+        // To reset index intialization at each frame so it wont get ahead or too low.
         if (indexLetter >= 3)
         {
             indexLetter = 3;
@@ -33,9 +42,48 @@ public class SwitchLetter : MonoBehaviour
         if (indexLetter <= 0)
         {
             indexLetter = 0; 
-        }    
-                            
+        }        
+
+        //Update score on load
+        //Have user attempt the exercise before?
+        if (newIndexTrigger > indexLetter)
+        {                
+            // To set star object as active
+            // Level 1
+            if(newIndexTrigger < 5)
+            {
+                for(int i = 0; i < newIndexTrigger; i++)
+                {
+                progressStarLetter[i].gameObject.SetActive(true);
+                }  
+            }
+
+            // Level 2
+            else if(newIndexTrigger > 4 && newIndexTrigger < 8)
+            {   
+                // Turning off level 1 star
+                for(int y = 0; y <= 4; y++)
+                {
+                    progressStarLetter[y].gameObject.SetActive(false);
+                }
+                // Turning on level 2 star
+                for(int z = 4; z < newIndexTrigger; z++)
+                {
+                    progressStarLetter[z].gameObject.SetActive(true);
+                }
+            }       
+        }
     }
+
+    void OnDestroy()
+    {
+        //This will happen whenever this object is destroyed, which includes scene changes
+        // as well as existing the programs.
+        Debug.Log ("Scene Object was destroyed.");
+
+        PlayerPrefs.SetInt("LetterScore", newIndexTrigger);
+    }
+
     
 
     public void NextLetter()
@@ -63,6 +111,10 @@ public class SwitchLetter : MonoBehaviour
             letterNavigationBtn[0].gameObject.SetActive(true);
             letterNavigationBtn[1].gameObject.SetActive(true); 
         }
+
+        indexPrevious = indexLetter - 1;
+
+        hijaiyahLetterSound[indexPrevious].gameObject.SetActive(false);
 
         Debug.Log(indexLetter);
     }
@@ -92,6 +144,10 @@ public class SwitchLetter : MonoBehaviour
             letterNavigationBtn[1].gameObject.SetActive(true); 
         }
 
+        indexPrevious = indexLetter + 1;
+
+        hijaiyahLetterSound[indexPrevious].gameObject.SetActive(false);
+
         Debug.Log(indexLetter);
     }
 
@@ -101,10 +157,22 @@ public class SwitchLetter : MonoBehaviour
         {
             hijaiyahLetterSound[i].gameObject.SetActive(false);
             hijaiyahLetterSound[indexLetter].gameObject.SetActive(true);
+            
+            hijaiyahLetter[indexLetter].gameObject.SetActive(false);
         }
 
-        hijaiyahLetterSound[indexLetter].Play();
+    }
 
+    public void closeRecitationLetterOnClick()
+    {
+        hijaiyahLetterSound[indexLetter].gameObject.SetActive(false);
+            
+        hijaiyahLetter[indexLetter].gameObject.SetActive(true);
+    }
+
+    public void playRecitationLetterOnClick()
+    {
+        recitationLetterVideo[indexLetter].Play();
     }
 
     public void RestartOnClick()
@@ -112,6 +180,49 @@ public class SwitchLetter : MonoBehaviour
         Destroy (hijaiyahLetter[indexLetter]);
         hijaiyahLetter[indexLetter] = Instantiate (prefabLetter[indexLetter]);
         hijaiyahLetter[indexLetter].gameObject.SetActive(true);
+
+        hijaiyahLetterSound[indexLetter].gameObject.SetActive(false);
+
     }
 
+    public void OnTriggerEnter2D (Collider2D collider)
+    {   
+        //Load data from player prefs.
+        newIndexTrigger = PlayerPrefs.GetInt("LetterScore", 0);
+
+        // To check if user is attempting for first time    
+        if (newIndexTrigger == indexLetter)
+        {
+            progressDialogueBox[newIndexTrigger].gameObject.SetActive(true);    
+            
+            // To set star object as active
+            // Level 1
+            if(newIndexTrigger < 5)
+            {
+                for(int i = 0; i < newIndexTrigger; i++)
+                {
+                progressStarLetter[i].gameObject.SetActive(true);
+                }  
+            }
+
+            // Level 2
+            else if(newIndexTrigger > 4 && newIndexTrigger < 8)
+            {   
+                // Turning off level 1 star
+                for(int y = 0; y <= 4; y++)
+                {
+                    progressStarLetter[y].gameObject.SetActive(false);
+                }
+                // Turning on level 2 star
+                for(int z = 4; z < newIndexTrigger; z++)
+                {
+                    progressStarLetter[z].gameObject.SetActive(true);
+                }
+            }         
+        }
+
+        newIndexTrigger++;
+        PlayerPrefs.SetInt("LetterScore", newIndexTrigger);
+        Debug.Log("Trigger!");
+    }
 }
