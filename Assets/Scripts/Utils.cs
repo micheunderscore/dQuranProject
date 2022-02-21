0d76b8ac961b18b3c2a81602d01fd0f7e90e1b0f
Miche_ <51939599+micheunderscore@users.noreply.github.com>
2022-02-22 02:36:31 +0800

    fix: attempt 1 @ fix android load streaming assets

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,10 +1,30 @@
 using System.IO;
+using System.Collections;
+using UnityEngine.Networking;
 using UnityEngine;
 public class JsonReader {
+    private string passedString;
     public string Read (string route) {
-        string path = Application.dataPath + route;
-        string jsonString = File.ReadAllText(path);
+        string filePath = Path.Combine(Application.streamingAssetsPath + Path.DirectorySeparatorChar, route);
+        string jsonString = "";
+
+        Debug.Log ("UNITY:" + System.Environment.NewLine + filePath);
+
+        if (filePath.Contains ("://") || filePath.Contains (":///")) {
+            ReadFromAndroid(filePath);
+            jsonString = passedString;
+        } else {
+            jsonString = File.ReadAllText(filePath);
+        }
+
         return(jsonString);
+    }
+
+    public IEnumerable ReadFromAndroid (string filePath) {
+        UnityWebRequest www = UnityWebRequest.Get(filePath);       
+        yield return www.SendWebRequest();
+		passedString = www.downloadHandler.text;
+        Debug.Log("JSON DATA::: " + passedString);
     }
 
     public string GetUsername () {
