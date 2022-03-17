cc5370fef6e28f7a35474d752f1933064c41d0f1
achengg <97327123+ach3ngg@users.noreply.github.com>
2022-03-18 06:10:58 +0800

    Parallax Enabled
    New Background Template with Parallax Enabled

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -7,30 +7,28 @@ public class Parallax : MonoBehaviour
 {
     public float speed = 4f;
     private Vector3 startPosition;
-
-    public GameObject foreground;
-    public GameObject duplicate;
-
+    private Vector3 recordPosition;
 
     void Start()
     {
         startPosition = transform.position;
+        Debug.Log("Your start position is " + startPosition );
     }
 
     void Update()
     {
         transform.Translate(translation:Vector3.left*speed*Time.deltaTime);
 
-        if (transform.position.x > -72 && transform.position.x < -74 )
-        {
-            duplicate = Instantiate(foreground, new Vector3(37, 0, 0), Quaternion.identity);
+        if (transform.position.x <= -136.53f )
+        {   
+            recordPosition = transform.position;
+
+            Debug.Log("Go Away " + recordPosition);
+
+            transform.position = startPosition;
+
         }
 
-        if (transform.position.x == -110.99)
-        {
-            Destroy (duplicate);
-            transform.position = startPosition;
-        }
     }
 }
 
